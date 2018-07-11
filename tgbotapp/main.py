902adfa2ea6ad2a7767cbac6a2ac5c909a0900ee
krylov <ilya.krylov@gmail.com>
2018-07-11 18:53:29 +0300

    Add support of configs

--- tgbotapp/main.py
+++ tgbotapp/main.py
@@ -1,14 +1,38 @@
-import telebot
-from . import config
+from argparse import ArgumentParser
+from configparser import ConfigParser
+from os import environ
+from os.path import join
+from telebot import TeleBot
 
 
-bot = telebot.TeleBot(config.token)
+BOT_CONFIGS_ENV = "TG_CONFIG_PATH"
+
+
+def create_bot():
+    path = environ.get(BOT_CONFIGS_ENV)
+    if not path:
+        raise Exception("The '{}' environment variable should "
+                        "be declared.".format(BOT_CONFIGS_ENV))
+    parser = ArgumentParser()
+    parser.add_argument("-b", "--bot", help="The name of bot that will be run",
+                        default='')
+    args = parser.parse_args()
+    if not args.bot:
+        raise Exception("The bot name shouldn't be empty.")
+    full_bot_path = join(path, args.bot) + ".ini"
+    config = ConfigParser()
+    try:
+        config.read(full_bot_path)
+        token = config.get('main', 'token')
+    except Exception as exc:
+        raise exc
+    return TeleBot(token)
+
+bot = create_bot()
 
 
 @bot.message_handler(commands=['trn'])
 def handle_start_help(message):
-    import pdb
-    pdb.set_trace()
     pass
 
 
